010ff13b9bbbf7275df0fb19ca4ab93145b3e3ff
toannh <toannh@exoplatform.com>
2014-11-28 17:32:34 +0700

    ECMS-6518: Open Document in Office. Implement add Open... button

--- platform-ui-web-eXoResources/src/main/webapp/javascript/eXo/webui/UIRightClickPopupMenu.js
+++ platform-ui-web-eXoResources/src/main/webapp/javascript/eXo/webui/UIRightClickPopupMenu.js
@@ -139,8 +139,30 @@
 	  clickRightMouse : function(event, elemt, menuId, objId, whiteList, opt) {
 	    if (!event)
 	      event = window.event;
-	
-	    var contextMenu = document.getElementById(menuId);
+
+      $.ajax({
+        url: "/portal/rest/office/test?objId=" + objId,
+        dataType: "text",
+        type: "GET",
+        async: false
+        //	timeout:1000 * 10
+      })
+      .success(function (data) {
+        data = $.parseJSON(data);
+        var openDocument = $(".uiIconEcmsOpenDocument").parent();
+        var html = "<i class=\"uiIconEcmsOpenDocument "+data.type+"\"></i>";
+
+        if(data.type==="Open_Word")
+          html+=eXo.ecm.WCMUtils.getBundle('OpenInOfficeConnector.label.open-in-word', eXo.env.portal.language);
+        if(data.type==="Open_Excel")
+          html+=eXo.ecm.WCMUtils.getBundle('OpenInOfficeConnector.label.open-in-excel', eXo.env.portal.language);
+        if(data.type==="Open_Powerpoint")
+          html+=eXo.ecm.WCMUtils.getBundle('OpenInOfficeConnector.label.open-in-powerpoint', eXo.env.portal.language);
+        openDocument.html(html);
+        openDocument.attr("onclick", "eXo.ecm.OpenDocumentInOffice.test('"+data.filePath+"')");
+      });
+
+      var contextMenu = document.getElementById(menuId);
 	    contextMenu.objId = objId;
 	
 	    //help to disable browser context menu
